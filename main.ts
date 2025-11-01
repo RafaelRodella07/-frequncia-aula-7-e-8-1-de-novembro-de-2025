7f4d10b45cdba17aeb79971f9de64dfaf2d97cd8
RafaelRodella07 <00001110529880sp@al.educacao.sp.gov.br>
2025-11-01 15:59:43 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -3,10 +3,10 @@ input.onButtonPressed(Button.A, function () {
     indice += 1
     if (indice > 3) {
         basic.showIcon(IconNames.Yes)
-        lista = 0
+        indice = 0
     }
 })
-let lista = 0
+let lista: number[] = []
 let indice = 0
 indice = 0
 lista = [
